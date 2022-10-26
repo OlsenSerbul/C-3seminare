1367ee768d9f990e560c0717ae5c66057bde133c
Olga Serbul <Olsen.zika@mail.ru>
2022-10-26 19:06:10 +0300

    Example01_завершен_project

--- Example01/Program.cs
+++ Example01/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y),
+// причем Х не равно 0 и Y не равно 0 и выдает номер четверти плоскости, в которой находится эта точка.
+int GetPointArea(int coordX, int coordY)
+{
+    int numberArea = 0;
+    if (coordX > 0 && coordY > 0)
+    {
+        numberArea = 1;
+    }
+    if (coordX < 0 && coordY > 0)
+    {
+        numberArea = 2;
+    }
+    if (coordX < 0 && coordY < 0)
+    {
+        numberArea = 3;
+    }
+    if (coordX > 0 && coordY < 0)
+    {
+        numberArea = 4;
+    }
+    return numberArea;
+}
+Console.WriteLine("Введите X:  ");
+int X = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Y: ");
+int Y = Convert.ToInt32(Console.ReadLine());
+
+int numberArea =  GetPointArea(X , Y);
+Console.WriteLine(numberArea);
