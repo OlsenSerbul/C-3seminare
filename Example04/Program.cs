30afe05586657a04a27ec26b4be02dc2683f165e
Olga Serbul <Olsen.zika@mail.ru>
2022-10-26 22:01:39 +0300

    Example04_project

---
+++ Example04/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на входе число (N) 
+// и выдает таблицу квадратов чисел от 1 до N.
+
+void FindSquares( int N )
+{
+    int squareN = 0;
+    for (int i = 1; i <= N ; i++ )
+    {
+        squareN = i * i;
+        Console.WriteLine(squareN);
+    }
+}
+
+Console.WriteLine("Введите число N : ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+
+FindSquares( N );
