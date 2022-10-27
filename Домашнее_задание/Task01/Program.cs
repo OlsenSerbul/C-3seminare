e8d052ed5f3b204a84ee098acab28378ef6883a5
Olga Serbul <Olsen.zika@mail.ru>
2022-10-27 10:55:06 +0300

    Task01_переделанно

--- Домашнее_задание/Task01/Program.cs
+++ Домашнее_задание/Task01/Program.cs
@@ -1,6 +1,5 @@
 ﻿// Напишите программу, которая принимает на вход пятизначное 
 // число и проверяет, является ли оно палиндромом.
-
 string FindPol(int number1, int number2, int number4, int number5)
 {
     string answer = "";
@@ -14,25 +13,21 @@ string FindPol(int number1, int number2, int number4, int number5)
     }
     return answer;
 }
-
 Console.WriteLine("Введите пятизначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-int number1 = number % 10;
-int number2 = (number /10) % 10;
-int number3 = (number /100) % 10;
-int number4 = (number /1000) % 10;
-int number5 = number /10000;
-
-while (number > 0)
+if (number < 0)
 {
-    if (number >= 10000 && number < 100000)
-    {
-       string answer = FindPol(number1,number2,number4,number5);
-       Console.WriteLine(answer);
-    }
-    else
-    Console.WriteLine("Вы ввели не пятизначное число");
+   Console.WriteLine("FALSE");
 }
-
-Console.WriteLine("false");
-
+if (number >= 10000 && number < 100000)
+    {
+      int number1 = number % 10;
+      int number2 = (number /10) % 10;
+      int number3 = (number /100) % 10;
+      int number4 = (number /1000) % 10;
+      int number5 = number /10000;
+      string answer = FindPol(number1,number2,number4,number5);
+      Console.WriteLine(answer);
+    }
+else  
+    Console.WriteLine("Вы ввели не пятизначное число");
