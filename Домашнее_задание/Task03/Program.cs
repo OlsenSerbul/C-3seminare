baa35d81efd387bc21d2e78c85285642a42bcfaf
Olga Serbul <Olsen.zika@mail.ru>
2022-10-27 11:10:34 +0300

    Task03

---
+++ Домашнее_задание/Task03/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число (N) и 
+// выдаёт таблицу кубов чисел от 1 до N.
+void FindCube ( int N )
+{
+    int cubeN = 0;
+    for (int i = 1; i <= N ; i++ )
+    {
+        cubeN = i * i * i;
+        Console.WriteLine(cubeN);
+    }
+}
+
+Console.Write("Введите число N : ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+
+FindCube( N );
